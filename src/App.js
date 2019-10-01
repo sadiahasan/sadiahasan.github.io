b8df7128d5c35b3edae408649982647f60e4b056
Hasan, Sadia A <sadia.a.hasan@accenture.com>
2019-10-01 15:32:05 -0400

    typewriter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Typewriter from 'typewriter-effect';
+import Typist from 'react-typist';
+import page2 from './page2';
 
 function App() {
+  var i = 0;
+  var txt = 'Lorem ipsum dummy text blabla.';
+  var speed = 50;
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Site under construction!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typist>
+          <div className="flex-container">
+            <h1 className="header1"> Hello World! </h1>
+            <h4 className="header2"> my name is sadia hasan and i'm a software engineer </h4>
+          </div>
+        </Typist>
       </header>
+      {page2}
     </div>
   );
 }
---
+++ src/page2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typist from 'react-typist';
+import './App.css';
+
+function page2() {
+
+  return (
+    <div> hello </div>
+  );
+}
+
+export default page2;
